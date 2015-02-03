e4e7d273b416e589b35b561a0cd07bcef0bab0f1
Joaquin Bravo Contreras <jackbravo@gmail.com>
2015-02-03 01:47:18 +0000

    Set minDate depending on which is first, minDate or the first selected date

--- js/date_multiselect.js
+++ js/date_multiselect.js
@@ -8,14 +8,30 @@ Drupal.behaviors.date_multiselect = {
     for (var id in Drupal.settings.dateMultiselect) {
       var input = $('input#' + id);
       if (!input.hasClass('date-multiselect-init')) {
-        dateMultiselect = Drupal.settings.dateMultiselect[id].settings;
+        settings = Drupal.settings.dateMultiselect[id].settings;
+        if (settings.hasOwnProperty('minDate') && settings.hasOwnProperty('addDates')) {
+          settings.minDate = getMinDate(settings.minDate, settings.addDates[0], settings.dateFormat);
+        }
         input
           .addClass('date-multiselect-init')
           .wrap('<div id="' + id + '-wrapper" />')
-          .parent().multiDatesPicker(dateMultiselect)
+          .parent().multiDatesPicker(settings)
         input.hide();
       }
     }
   }
 };
 })(jQuery);
+
+function getMinDate(minDate, firstDate, format) {
+  if (typeof minDate == 'string') {
+    var minDateObj = jQuery.datepicker.parseDate(format, minDate);
+  } else if (typeof minDate == 'number') {
+    var minDateObj = new Date();
+    minDateObj.setDate(minDateObj.getDate() + minDate);
+  }
+
+  var firstDateObj = jQuery.datepicker.parseDate(format, firstDate);
+
+  return (firstDateObj > minDateObj) ? minDate : firstDate;
+}
